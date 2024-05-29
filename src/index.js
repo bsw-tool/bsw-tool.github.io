39077f8b2ad4a57a226d84cbe1d527cd12c49fba
Bayu Santoso Widodo <110602582+bayu-sw@users.noreply.github.com>
2024-05-29 12:33:37 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ class MyApp extends App{
 
   _main(){
     super();
+    // Get a reference to the <body> element
+    const body = document.body;
+
+    // Remove all child nodes from the <body> element
+    while (body.firstChild) {
+      body.removeChild(body.firstChild);
+    }
   }
 
 }
